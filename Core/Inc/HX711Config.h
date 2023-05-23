1e4d65db551b7325dd616808fbd7fab88951cd8c
nhatVNU <tranminhnhat20022002@gmail.com>
2023-05-24 00:52:03 +0700

    first commit

---
+++ Core/Inc/HX711Config.h
@@ -0,0 +1,14 @@
+/*
+ * hx711Config.h
+ *
+ *  Created on: May 23, 2023
+ *      Author: tranm
+ */
+
+#ifndef INC_HX711CONFIG_H_
+#define INC_HX711CONFIG_H_
+
+#define	_HX711_USE_FREERTOS		0
+#define _HX711_DELAY_US_LOOP	4
+#define __nop() asm("NOP")
+#endif /* INC_HX711CONFIG_H_ */
